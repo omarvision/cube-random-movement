c59513283b4be270349cf1a15043eff2da584d24
Omar Santiago <omarkenosia@gmail.com>
2021-07-13 10:13:40 -0400

    Add files via upload

---
+++ CubeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //for NavMeshAgent
+
+public class CubeScript : MonoBehaviour
+{
+    private NavMeshAgent nma = null;
+    private GameObject[] RandomPoint;
+    private int CurrentRandom;
+
+    private void Start()
+    {
+        nma = this.GetComponent<NavMeshAgent>();
+        RandomPoint = GameObject.FindGameObjectsWithTag("RandomPoint");
+        Debug.Log("RandomPoints = " + RandomPoint.Length.ToString());
+    }
+
+    private void Update()
+    {
+        if (nma.hasPath == false)
+        {
+            CurrentRandom = Random.Range(0, RandomPoint.Length + 1);
+            nma.SetDestination(RandomPoint[CurrentRandom].transform.position);
+            Debug.Log("Moving to RandomPoint " + CurrentRandom.ToString());
+        }
+    }
+}
